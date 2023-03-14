ac267644eae403987572b801c64aaec0b5d7a895
Diego Andrés Arango Díaz <73032505+yodaad@users.noreply.github.com>
2023-03-14 15:56:08 -0500

    Final version added

---
+++ app/main.py
@@ -0,0 +1,22 @@
+import utils
+import read_csv
+import charts
+
+
+
+def run(): 
+    
+    data = read_csv.read_csv("./data.csv")
+    country = input("Type country: ").title()
+    
+    result = utils.population_by_country(data, country)
+    
+    if len(result) > 0:
+        country = result[0]
+        labels, values = utils.get_population(country)
+        charts.generate_bar_chart(labels, values)
+    
+    print(result)
+
+if __name__ == "__main__":
+    run()
